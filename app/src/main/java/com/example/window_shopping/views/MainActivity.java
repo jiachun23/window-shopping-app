e86d95835325a81baf47c17e9802ef741849ef09
JC <t.jiachun23@gmail.com>
2021-08-04 03:48:54 +0800

    Enhanced UI

--- app/src/main/java/com/example/window_shopping/views/MainActivity.java
+++ app/src/main/java/com/example/window_shopping/views/MainActivity.java
@@ -12,10 +12,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.window_shopping.R;
 import com.example.window_shopping.models.CartItem;
 import com.example.window_shopping.viewmodels.ShopViewModel;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -24,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     NavController navController;
     ShopViewModel shopViewModel;
+
+    private int cartQuantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,12 @@ public class MainActivity extends AppCompatActivity {
         shopViewModel.getCart().observe(this, new Observer<List<CartItem>>() {
             @Override
             public void onChanged(List<CartItem> cartItems) {
-                Log.d(TAG, "onChanged: "+ cartItems.size());
+                int quantity= 0;
+                for (CartItem cartItem: cartItems){
+                    quantity += cartItem.getQuantity();
+                }
+                cartQuantity = quantity;
+                invalidateOptionsMenu();
             }
         });
     }
@@ -50,6 +61,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.cartFragment);
+        View actionView = menuItem.getActionView();
+
+        TextView cartBadgeTextView = actionView.findViewById(R.id.cart_badge_text_view);
+
+        cartBadgeTextView.setText(String.valueOf(cartQuantity));
+        cartBadgeTextView.setVisibility(cartQuantity == 0 ? View.GONE : View.VISIBLE);
+
+        actionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(menuItem);
+            }
+        });
+
         return true;
     }
 
